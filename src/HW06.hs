ef3d1d433db4c9a15b4f3022196e9023935b82dd
ParkJuhyeong <majecty@gmail.com>
2015-01-13 11:54:30 +0900

    Implement parseData and parseMarket.

--- src/HW06.hs
+++ src/HW06.hs
@@ -4,9 +4,9 @@ module HW06 where
 
 import Data.Aeson
 -- import Data.Monoid
--- import GHC.Generics
+import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as B
--- import qualified Data.Text                  as T
+import qualified Data.Text                  as T
 -- import qualified Data.Text.IO               as T
 
 ynToBool :: Value -> Value
@@ -18,3 +18,27 @@ ynToBool other = other
 
 parseData :: B.ByteString -> Either String Value
 parseData input = fmap ynToBool $ eitherDecode input
+
+data Market = Market { marketname :: T.Text
+                    ,  x :: Float
+                    ,  y :: Float
+                    ,  state :: T.Text }
+  deriving (Show, Generic, Eq)
+
+instance FromJSON Market
+
+resultToEither :: Result a -> Either String a
+resultToEither (Error errorMsg) = Left errorMsg
+resultToEither (Success a) = Right a
+
+parseMarkets :: B.ByteString -> Either String [Market]
+parseMarkets input = do
+  parsedValue <- parseData input
+  markets <- resultToEither $ fromJSON parsedValue
+  return markets
+
+loadData :: IO [Market]
+loadData = do
+  filedata <- B.readFile "markets.json"
+  let parseResult = parseMarkets filedata
+  either fail return parseResult
