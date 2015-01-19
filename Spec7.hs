f989ab166910d3302eb8315c4dd6ed5a53377981
ParkJuhyeong <majecty@gmail.com>
2015-01-19 23:00:23 +0900

    Add tests for fibs1 and fibs2.

--- Spec7.hs
+++ Spec7.hs
@@ -16,3 +16,11 @@ main = hspec $ do
       fib 6 `shouldBe` 8
     it "10 -> 15" $ do
       fib 10 `shouldBe` 55
+
+  describe "fibs1" $ do
+    it "start with 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377" $ do
+      take 15 fibs1 `shouldBe` [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377]
+
+  describe "fibs2" $ do
+    it "is same with fibs1" $ do
+      take 15 fibs1 `shouldBe` take 15 fibs2
