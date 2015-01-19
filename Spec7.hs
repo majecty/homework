fdd795808c483d42761f0ed5c49eb003a7d28625
ParkJuhyeong <majecty@gmail.com>
2015-01-19 21:57:24 +0900

    Add simple test7.

--- Spec7.hs
+++ Spec7.hs
@@ -10,3 +10,9 @@ main = hspec $ do
   describe "Prelude.heade" $ do
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
+
+  describe "fibonaccia" $ do
+    it "6 ->  8" $ do
+      fib 6 `shouldBe` 8
+    it "10 -> 15" $ do
+      fib 10 `shouldBe` 55
