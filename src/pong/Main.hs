7b51ff453e00a43a45069e224feb437024db3015
MSapiens <kiki9014@gmail.com>
2015-03-08 16:10:40 +0900

    Make default update function.

--- src/pong/Main.hs
+++ src/pong/Main.hs
@@ -2,8 +2,25 @@
 
 module Main (main) where
 
-import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+
+data World = World { field :: Float }
+
+initialWorld :: World
+initialWorld = World { field = 0 }
+
+step :: Float -> World -> World
+step elapsed w@(World { field = fieldValue }) = World { field = fieldValue + elapsed }
+
+react :: Event -> World -> World
+react ev w@(World { field = fieldValue }) = w
+
+render :: World -> Picture
+render (World { field      = fieldValue }) = Circle fieldValue
 
 main :: IO ()
-main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
+main = do
+  let world = initialWorld
+  play (InWindow "Pong" (400, 300) (10, 10))
+    white 30 world render react step
 
